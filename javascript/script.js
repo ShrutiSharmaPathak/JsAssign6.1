7f3bcaddf56666c957e99889041c1bdde3855de7
ShrutiSharmaPathak <ss_shruti@yahoo.com>
2018-08-01 22:59:13 +0530

    first commit of javascript

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+/* This is external java script file to be called any where  and any no of times */
+alert("I am from external file"); // Shows the alert box with defined message
+
+// <script> tag not required in .js file.
